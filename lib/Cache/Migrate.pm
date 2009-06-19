1b8fa7d9b18a9b333956be443768006973e2f644
mala <github@ma.la>
2009-06-19 19:00:15 +0900

    initial commit

---
+++ lib/Cache/Migrate.pm
@@ -0,0 +1,33 @@
+package Cache::Migrate;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Cache::Migrate -
+
+=head1 SYNOPSIS
+
+  use Cache::Migrate;
+
+=head1 DESCRIPTION
+
+Cache::Migrate is
+
+=head1 AUTHOR
+
+mala E<lt>cpan@ma.laE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
